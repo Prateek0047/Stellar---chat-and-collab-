504b077d4e01f99263dc6d065f09024453b13848
sumit-sumi <hsumit542@gmail.com>
2025-10-19 10:02:02 +0530

    Initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,16 @@ import express from "express";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
